baac0694044a7f71c38870d959a1ca8390565662
Prakash Verma <prakashverma861@gmail.com>
2025-11-25 13:36:43 +0530

    Updated GeneralFooter Link

--- src/components/GeneralFooter.jsx
+++ src/components/GeneralFooter.jsx
@@ -5,7 +5,7 @@ const GeneralFooter = () => {
         <footer className='relative mt-auto flex flex-col items-center text-gray-300 mb-6 gap-2 md:flex-row md:justify-evenly'>
             
             <div className='flex gap-x-4 md:order-last'>
-                <a className='text-4xl hover:text-primary-color hover:animate-bounce' target='_blank' href="https://github.com/TheVermaVD">
+                <a className='text-4xl hover:text-primary-color hover:animate-bounce' target='_blank' href="https://github.com/prakashverma-dev/">
                     <i className='bx bxl-github' ></i>
                 </a>
 
